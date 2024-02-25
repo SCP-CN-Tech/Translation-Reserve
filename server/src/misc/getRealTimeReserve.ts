0263dc5cfc604f904ba23dede4c81559fd20fdfa
Zokhoi <20432565+Zokhoi@users.noreply.github.com>
2024-02-25 22:18:35 +0800

    Disable reserving scp-cn pages

--- server/src/misc/getRealTimeReserve.ts
+++ server/src/misc/getRealTimeReserve.ts
@@ -8,36 +8,6 @@ export default async function getRealTimeReserve(
   res: Response,
   next: NextFunction
 ) {
-  let page = unixNamify(req.params.pageName);
-  let wikidotToken7 = Math.random().toString(36).substring(4);
-  let params = {
-    wikidot_token7: wikidotToken7,
-    callbackIndex: 0,
-    moduleName: "list/ListPagesModule",
-    category: "reserve",
-    name: page.substring(0,52),
-    order: "created_at desc",
-    seaparate: "false",
-    module_body: `
-    [[div_ id="created_by"]]
-    [[a href="https://www.wikidot.com/user:info/%%created_by_unix%%" target="_parent"]]%%created_by%%[[/a]]
-    [[/div]]
-    [[div_ id="created_at"]]
-    %%created_at|%Y-%m-%d|hover%%
-    [[/div]]`
-  }
-  let fetchbody = new URLSearchParams();
-  for (const key in params) { fetchbody.append(key, params[key]) }
-  let body = await (await fetch("http://scp-tech-cn.wikidot.com/ajax-module-connector.php", {
-    method: "POST",
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
-      Referer: 'Translation Reservation for SCP-CN',
-      Cookie: `wikidot_token7=${wikidotToken7};`,
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: fetchbody,
-  })).json();
   res.set({
     "Access-Control-Allow-Origin": req.headers.origin ?? "*",
     "Access-Control-Allow-Headers":
@@ -46,6 +16,7 @@ export default async function getRealTimeReserve(
     "Access-Control-Allow-Credentials": true,
     "Content-Type": "text/html",
   });
+
   let text = `<style>
     .transres-msg {
       text-align: center;
@@ -64,27 +35,70 @@ export default async function getRealTimeReserve(
       text-decoration: underline;
     }
   </style>`;
-  if (body.status!="ok") {
-    text += `<div class="transres-msg">Wikidot error ${body.status}: ${body.message}</div>`;
+
+  let page = unixNamify(req.params.pageName);
+
+  if (page.startsWith("scp-cn")) {
+    // No. Don't let them reserve it.
+    text += `<div class="transres-msg">
+      <h1>此頁不可預定翻譯</h1>
+    </div>`;
+
   } else {
-    // console.log(body)
-    let $ = load(body.body);
-    // console.log($(".list-pages-box").html())
-    if (!$(".list-pages-box").html()?.trim()) {
-      text += `<div class="transres-msg">
-        <h1>沒有找到與此頁相關的翻譯預定信息</h1>
-        <b><a href="http://scp-tech-cn.wikidot.com/reserve:${page}" target="_parent">按此</a></b>創建預定。
-      </div>`;
+
+    let wikidotToken7 = Math.random().toString(36).substring(4);
+    let params = {
+      wikidot_token7: wikidotToken7,
+      callbackIndex: 0,
+      moduleName: "list/ListPagesModule",
+      category: "reserve",
+      name: page.substring(0,52),
+      order: "created_at desc",
+      seaparate: "false",
+      module_body: `
+      [[div_ id="created_by"]]
+      [[a href="https://www.wikidot.com/user:info/%%created_by_unix%%" target="_parent"]]%%created_by%%[[/a]]
+      [[/div]]
+      [[div_ id="created_at"]]
+      %%created_at|%Y-%m-%d|hover%%
+      [[/div]]`
+    }
+    let fetchbody = new URLSearchParams();
+    for (const key in params) { fetchbody.append(key, params[key]) }
+    let body = await fetch("http://scp-tech-cn.wikidot.com/ajax-module-connector.php", {
+      method: "POST",
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:89.0) Gecko/20100101 Firefox/89.0',
+        Referer: 'Translation Reservation for SCP-CN',
+        Cookie: `wikidot_token7=${wikidotToken7};`,
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: fetchbody,
+    }).then(res=>res.json());
+
+    if (body.status!="ok") {
+      text += `<div class="transres-msg">Wikidot error ${body.status}: ${body.message}</div>`;
     } else {
-      // res.send(body.body)
-      // console.log($("#u-created_at").children("span").attr('class'))
-      let time = new Date(parseInt($("#u-created_at").children("span").attr('class').split(' ')[1].substring(5)+'000'));
-      text += `<div class="transres-msg">
-        <h1>此頁已被預定翻譯</h1>
-        由 ${$("#u-created_by").html()} 於 ${time.getFullYear()}-${("0"+(time.getMonth()+1)).slice(-2)}-${("0"+time.getDate()).slice(-2)}
-      </div>`;
+      // console.log(body)
+      let $ = load(body.body);
+      // console.log($(".list-pages-box").html())
+      if (!$(".list-pages-box").html()?.trim()) {
+        text += `<div class="transres-msg">
+          <h1>沒有找到與此頁相關的翻譯預定信息</h1>
+          <b><a href="http://scp-tech-cn.wikidot.com/reserve:${page}" target="_parent">按此</a></b>創建預定。
+        </div>`;
+      } else {
+        // res.send(body.body)
+        // console.log($("#u-created_at").children("span").attr('class'))
+        let time = new Date(parseInt($("#u-created_at").children("span").attr('class').split(' ')[1].substring(5)+'000'));
+        text += `<div class="transres-msg">
+          <h1>此頁已被預定翻譯</h1>
+          由 ${$("#u-created_by").html()} 於 ${time.getFullYear()}-${("0"+(time.getMonth()+1)).slice(-2)}-${("0"+time.getDate()).slice(-2)}
+        </div>`;
+      }
     }
   }
+
   res.send(text);
 }
 
